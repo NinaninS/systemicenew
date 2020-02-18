8ecf11c6b22554db097daf62a4033f7a2b7b0628
ninanin <luboputno@gmail.com>
2020-02-18 18:44:46 +0300

    file

---
+++ files/corp-popup.php
@@ -0,0 +1,52 @@
+
+		<div id="overlay-corp" class="overlay">
+
+			<div class="overlay-block">
+				
+			<img src="../images/close.png" alt="" class = "close-popup-corp close-popup-desktop">
+
+				<div class="popup-container">
+					
+					<img class="popup-img" src="../images/corp1.jpg">
+
+					<div class="popup-form">
+						
+						<div class="popup-form_title">
+							
+							<h2 class="h2-category">Написать в отдел групповых заездов</h2>
+
+							<p class="popup-title">Напишите нам и мы рассчитаем для Вас индивидуальное предложение</p>
+						</div>
+
+						<form>
+							<input type="hidden" name="hotel_id" value="2112">
+
+							<div class="popup-input">
+								<input class="popup-inputblock input" type="text" name="name" placeholder="Имя">
+							</div>
+
+							<div class="popup-input">
+								<input class="popup-inputblock input" class="phone-validated" type="tel" name="phone" placeholder="Телефон">
+							</div>
+
+							<div class="popup-input">
+								<input class="popup-inputblock input" type="text" name="email" placeholder="E-mail">
+							</div>
+
+							<div class="popup-input">
+								<textarea class="popup-inputblock input" name="textarea" placeholder="Ваше сообщение" rows="3"></textarea>
+							</div>
+
+							<div class="popup-input">
+								<button class="submit-style send-order" type="button">Отправить</button>
+							</div>
+
+							<p class="person-data">*Нажимая на кнопку, вы даете согласие на обработку персональных данных</p>
+
+						</form>
+						
+					</div>
+				</div>
+			</div>
+			
+		</div>
---
+++ files/header.php
@@ -0,0 +1,96 @@
+<div class="header">
+			
+			<div class="header-wrapper">
+				
+				<div class="left-header">
+					<a href="">
+						<img class="logo" src="../images/logo.png">
+						<img class="logo-min" src="../images/logomin.png">
+					</a>
+				</div>
+
+				<div class="menu-wrapper">
+
+					<div class="corp-wrapper-mob">
+						<a class="link-corp btn" href="../corp/">
+							Групповые заезды
+						</a>
+					</div>
+					
+					<ul class="menu-main">
+						<li class="menu-list">
+							<a href="../inclusive/">All inclusive</a>
+						</li>
+						<li class="menu-list">
+							<a href="../price/">Номера и цены</a>
+						</li>
+						<li class="menu-list">
+							<a href="../sale/">Акции</a>
+						</li>
+						<li class="menu-list" id="list_hover">
+							<a class="link_arrow" href=" ">Услуги</a>
+							<div class="header__submenu">
+								<ul class="header__submenu-inner">
+									<li class="">
+										<a class="" href="../food">Рестораны и бары</a>
+									</li>
+									<li class="">
+										<a class="" href="../spa">СПА-комплекс</a>
+									</li>
+									<li class="">
+										<a class="" href="../sport">Развлечение и спорт</a>
+									</li>
+								</ul>
+							</div>
+						</li>
+						<li class="menu-list">
+							<a href="../corp/">Корп.клиенты</a>
+						</li>
+						<li class="menu-list">
+							<a href="../contact/">Контакты</a>
+						</li>
+					</ul>
+
+					<div class="tel-wrapper-mob">
+						<a href="tel:74995208648" class="t-bron">8 (499) 520-86-48</a> <span>Частным лицам</span><br>
+						<a href="tel:74991107043" class="t-corp">8 (499) 110-70-43</a> <span>Групповые заезды</span>
+					</div>
+
+
+				</div>
+
+				<div class="phone_number_header_mob">
+					<a href="tel:+74993223196" class="t-bron" style="text-decoration: none;color: #fff;">8 (499) 322-31-96</a>
+				</div>
+
+				<div class="right-header">
+
+					<div class="tel-wrapper">
+						Частным лицам: <a href="tel:74995208648" class="t-bron">8 (499) 520-86-48</a><br>
+						Групповые заезды: <a href="tel:74991107043" class="t-corp">8 (499) 110-70-43</a>
+					</div>
+					
+					<div class="corp-wrapper">
+						<a class="link-corp btn" href="../corp/">
+							Групповые заезды
+						</a>
+					</div>
+					
+					<div class="corp-wrapper-corp">
+						<a class="link-corp btn corp-btn" type="button">
+							Написать в отдел групп. заездов
+						</a>
+					</div>
+					
+					
+
+					<div class="burger-menu-desktop">
+						<img class="burger_button_t" src="../images/menu.svg"> 
+						<img class="burger_button_t2" src="../images/close.svg">
+						<span class="menu_title">МЕНЮ</span>
+					</div>
+
+				</div>
+			</div>
+
+		</div>
