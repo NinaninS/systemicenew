2679b09605623cca37ec19bbfd65c8274cf34111
Evgeny <evgenij.baltais@gmail.com>
2020-03-12 12:52:17 +0300

    правки

---
+++ files/section.php
@@ -0,0 +1,66 @@
+<div class="content-box content-slide hidden-screen">
+    <div class="service-block clearfix">
+        <div class="img-wrapper">
+            <div class="service-img" style="background-image: url('../images/kt.jpg');"></div>
+        </div>
+        <div class="service-desc">
+            <h1 class="h1">КЕЙТЕРИНГ1</h1>
+            <div class="line"></div>
+            <p class="bl-desc">
+                Мероприятия любого масштаба со вкусом, стилем и атмосферой. Лучшие event события Москвы и других городов.
+            </p>
+            <div class="spy-left"></div>
+            <div class="spy-top"></div>
+            <div class="spy-right"></div>
+            <div class="spy-bottom"></div>
+        </div>
+    </div>
+    <div class="service-block clearfix">
+        <div class="img-wrapper">
+            <div class="service-img" style="background-image: url('../images/bgame.jpg');"></div>
+        </div>
+        <div class="service-desc">
+            <h1 class="h1">БИЗНЕС-ИГРЫ</h1>
+            <div class="line"></div>
+            <p class="bl-desc">
+                Бизнес-игры  отличная современная альтернатива тимбилдингу.
+            </p>
+        </div>
+        <div class="spy-left"></div>
+        <div class="spy-top"></div>
+        <div class="spy-right"></div>
+        <div class="spy-bottom"></div>
+    </div>
+    <div class="service-block clearfix">
+        <div class="img-wrapper">
+            <div class="service-img" style="background-image: url('../images/bt.jpg');"></div>
+        </div>
+        <div class="service-desc">
+            <h1 class="h1">BUSINESS TRAVEL</h1>
+            <div class="line"></div>
+            <p class="bl-desc">
+                Сервисы бронирования Systemice Group работают с отелями и авиак
+            </p>
+        </div>
+        <div class="spy-left"></div>
+        <div class="spy-top"></div>
+        <div class="spy-right"></div>
+        <div class="spy-bottom"></div>
+    </div>
+    <div class="service-block clearfix">
+        <div class="img-wrapper">
+            <div class="service-img" style="background-image: url('../images/ved.jpg');"></div>
+        </div>
+        <div class="service-desc">
+            <h1 class="h1">ВЕДУЩИЕ</h1>
+            <div class="line"></div>
+            <p class="bl-desc">
+                Мы подобрали для Вас лучших!
+            </p>
+        </div>
+        <div class="spy-left"></div>
+        <div class="spy-top"></div>
+        <div class="spy-right"></div>
+        <div class="spy-bottom"></div>
+    </div>
+</div>
